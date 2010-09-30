4e7e11847349075725312599c2c5bc580032c0d8
Craig Mills <Craig.mills@unep-wcmc.org>
2010-09-30 13:27:54 +0100

    added all sinatra and starspan files

---
+++ ecosystem_service.rb
@@ -0,0 +1,81 @@
+require 'rubygems'#comment out for deployment
+require 'sinatra'#comment out for deployment
+require 'rio'
+require 'starspan'
+
+#return summary of % water fallen with PAs
+post '/es/freshwater' do
+  content_type :json
+  
+  #if params[:geomtype] == "ESRI Shapefile"
+  json = raster_summary(params[:shp][:tempfile].path, "data/raster/freshwater/gl_pc_wc_fin").to_json
+  params[:callback] ? "#{params[:callback]} (#{json})" : json
+  
+end
+
+#return kba coverage (user will need to divide sum/pixel num)
+post '/es/kba' do
+  content_type :json
+  
+  #if params[:geomtype] == "ESRI Shapefile"
+  json = raster_summary(params[:shp][:tempfile].path, "data/raster/kba/kba_ras/kba_1").to_json
+  params[:callback] ? "#{params[:callback]} (#{json})" : json
+  
+ 
+end
+
+#return carbon in tonnes
+post '/es/carbon' do
+  content_type :json
+  
+  #if params[:geomtype] == "ESRI Shapefile"
+  json = raster_summary(params[:shp][:tempfile].path, "data/raster/carbon2010/carbon_1").to_json
+  params[:callback] ? "#{params[:callback]} (#{json})" : json
+  
+ 
+end
+
+
+#doing all the work by creating the folders hang off to the starspan class and returning the hash array
+def raster_summary geom_path, raster
+  
+  
+  #if params[:geomtype] == "ESRI Shapefile"
+  
+  
+  #create folder to hold shp
+  folder = "data/shp/#{Time.now.to_f.to_s.gsub!(".","")}"
+  Dir.mkdir(folder)
+ 
+  #unzip file
+  system  "unzip #{geom_path} -d #{folder}"
+    
+  shp_path = ""
+  #get path of shp
+  rio(folder).all.files('*.shp') { |f| shp_path = f.to_s }
+
+  #setup params for starspan
+  options = {:raster => raster, 
+              :vector => shp_path, 
+              :stats=>["sum"], 
+              :data_folder => "data3", 
+              :starspan => "starspan"}
+
+  #TODO: need a better way of waiting for the commands to be finished before carrying on....
+  starttime = Time.now
+  until rio("#{File.basename(shp_path, ".shp")}.shx").exist?() || (Time.now - starttime) > 20
+    sleep 0.1
+  end
+
+
+  begin
+    summary = Starspan.new(options)
+    puts summary.to_s
+    out = summary.run_starspan
+    
+  rescue Exception => e
+    
+  end
+end
+
+
---
+++ starspan.rb
@@ -0,0 +1,102 @@
+require 'rubygems'#comment out for deployment
+require 'json'
+require 'rio'
+require 'fastercsv'
+
+
+class Starspan
+  
+  #process, grab all variables, setup folders for sticking the data in, check on locations of rasters and vectors
+    
+  
+  def initialize options = {}
+    
+    @raster = options[:raster]
+    @vector = options[:vector]
+    @stats = options[:stats]
+    @data_folder = options[:data_folder]
+    @starspan = options[:starspan]
+   
+    
+    #raise error if invalid folders and rasters
+    check_stuff 
+    
+    #set up all the places to put the files
+    setup_folders    
+    
+  end
+  
+  
+private
+  def setup_folders
+    dirs = ['csv', 'geojson']
+
+    #first check the data folder exists if not add a new one
+    
+    puts @data_folder.to_s  
+    
+    unless File.exists? @data_folder
+      Dir.mkdir("#{@data_folder}")
+      dirs.each {|dir| Dir.mkdir("#{@data_folder}/#{dir}") } 
+    end
+    
+        
+  end
+  
+  def check_stuff
+  
+    #is raster/vector valid?
+     raise "don't recognise raster" unless `starspan --raster #{@raster} --report`.lines.to_a[1].include? 'RASTER'
+     raise "don't recognise vector" unless `starspan --vector #{@vector} --report`.lines.to_a[1].include? "VECTOR"    
+    
+  end
+  
+  def csvtohash csv_path
+    #turn csv into a hash then json (will only ever be one header row and one data row- TODO: change for multiple rasters for now)
+   
+    multiout = []
+    FasterCSV.foreach(csv_path, :headers => :first_row) do |row|
+      output = {}
+      row.each { |hed, val|
+        output[hed] = val
+      }
+      
+     multiout << output 
+      
+    end
+    
+    multiout
+  end
+  
+  
+public
+
+
+  def run_starspan
+    
+    #create new file id for the process using current time in microseconds
+    @fileid = Time.now.to_f.to_s.gsub!(".","")
+  
+    system  "#{@starspan} --vector #{@vector} --raster #{@raster} --stats #{@data_folder}/csv/#{@fileid}.csv #{@stats.join(',')}"
+    
+    #TODO: make the command running better - try open4
+    starttime = Time.now
+    until rio("#{@data_folder}/csv/#{@fileid}.csv").exist?() || (Time.now - starttime) > 20
+      sleep 0.1
+    end
+
+    #read file here and return the value of the output as a hash
+    if rio("#{@data_folder}/csv/#{@fileid}.csv").exist?()    
+      #unpackage csv and return as json with the 
+        out = csvtohash("#{@data_folder}/csv/#{@fileid}.csv")
+    else
+        out = "no_csv"
+    end
+  
+  
+  end
+  
+  
+  
+  
+end
